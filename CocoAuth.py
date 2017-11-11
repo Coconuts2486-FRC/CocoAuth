de53a9f7dbfc25a02278ce5a9cc2008c4538e617
Zach Smith <kerbonauts@gmail.com>
2017-11-11 16:43:28 -0700

    Reorganized code to add initialization method.

--- CocoAuth.py
+++ CocoAuth.py
@@ -21,6 +21,28 @@ def doesDocExist():
         doc.share('apps@team2486.org', perm_type='user', role='writer')
         print("Could not find a document for " + str(now.year) + ". Creating...")
 
+# assign the id sheet
+def setIDSheet():
+    global idSheet
+    try:
+        idSheet = client.open("ID Sheet")
+        print("Found the ID sheet.")
+    except gspread.exceptions.SpreadsheetNotFound:
+        idSheet = client.create("ID Sheet")
+        idSheet.share('zachs@team2486.org', perm_type='user', role='writer')
+        idSheet.share('apps@team2486.org', perm_type='user', role='writer')
+        print("Could not find the ID sheet. Creating...")
+        
+
+# format a sheet
+def formatSheet():
+    sheet.update_cell(1, 1, "Date")
+    sheet.update_cell(1, 2, "ID")
+    sheet.update_cell(1, 3, "Person")
+    sheet.update_cell(1, 4, "Time In")
+    sheet.update_cell(1, 5, "Time Out")
+    print("Formatted the current sheet.")
+
 # determine if the doc contains the month
 def doesSheetExist():
     global sheet
@@ -30,15 +52,23 @@ def doesSheetExist():
     except gspread.exceptions.WorksheetNotFound:
         sheet = doc.add_worksheet(str(now.month), rows="800", cols="10")
         print("Could not find a worksheet for month " + str(now.month) + ". Creating...")
+        formatSheet()
 
-# formats a sheet
-def formatSheet():
-    sheet.update_cell(1, 1, "Person")
-    sheet.update_cell(1, 2, "Date")
+# initialization phase
+def initialize():
+    print("- Initialization phase -")
+    doesDocExist()
+    doesSheetExist()
+    setIDSheet()
 
-def addData():
-    
+# declare
+
+
+# add data
+def addData(person, ID):
+    print("Adding person.")
+    print(" - Name: " + person)
+    print(" - ID: " + ID)
 
 # runtime code goes here
-doesDocExist()
-doesSheetExist()
+initialize()
